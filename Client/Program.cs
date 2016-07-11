91103eadf43df9499abcc5532f5bf54896963d1d
xqlcrystal <xql252660@163.com>
2016-07-11 19:48:22 +0800

    第一节完成

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,13 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Client.WcfServices.Contracts;
+using Contracts;
+using System.ServiceModel;
 namespace Client
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //CallByWebRef();
+            //CallByCode();
+            using(ChannelFactory<ICalculator> ChannelFactory=new ChannelFactory<ICalculator>("calculatorservice"))
+            {
+                ICalculator proxy = ChannelFactory.CreateChannel();
+                using (proxy as IDisposable)
+                {
+                    Console.WriteLine("x + y ={2} when x={0} and y={1}", 1, 2, proxy.Add(1, 2));
+                    Console.WriteLine("x - y ={2} when x={0} and y={1}", 1, 2, proxy.Subtract(1, 2));
+                    Console.WriteLine("x * y ={2} when x={0} and y={1}", 1, 2, proxy.Multiply(1, 2));
+                    Console.WriteLine("x / y ={2} when x={0} and y={1}", 1, 2, proxy.Divide(1, 2));
+                }
+            }
+        }
+
+        private static void CallByCode()
+        {
+            using (ChannelFactory<ICalculator> ChannelFactory = new ChannelFactory<ICalculator>(new WSHttpBinding(), "http://127.0.0.1:9999/CalculatorService"))
+            {
+                ICalculator proxy = ChannelFactory.CreateChannel();
+                using (proxy as IDisposable)
+                {
+                    Console.WriteLine("x + y ={2} when x={0} and y={1}", 1, 2, proxy.Add(1, 2));
+                    Console.WriteLine("x - y ={2} when x={0} and y={1}", 1, 2, proxy.Subtract(1, 2));
+                    Console.WriteLine("x * y ={2} when x={0} and y={1}", 1, 2, proxy.Multiply(1, 2));
+                    Console.WriteLine("x / y ={2} when x={0} and y={1}", 1, 2, proxy.Divide(1, 2));
+                }
+            }
+        }
+
+        private static void CallByWebRef()
+        {
+            using (CalculatorServiceClient proxy = new CalculatorServiceClient())
+            {
+                Console.WriteLine("x + y ={2} when x={0} and y={1}", 1, 2, proxy.Add(1, 2));
+                Console.WriteLine("x - y ={2} when x={0} and y={1}", 1, 2, proxy.Subtract(1, 2));
+                Console.WriteLine("x * y ={2} when x={0} and y={1}", 1, 2, proxy.Multiply(1, 2));
+                Console.WriteLine("x / y ={2} when x={0} and y={1}", 1, 2, proxy.Divide(1, 2));
+
+            }
         }
     }
 }
---
+++ Hosting/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using Contracts;
+using Services;
+using System.ServiceModel.Description;
+namespace Hosting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HostByCode();
+
+            using(ServiceHost host=new ServiceHost(typeof(CalculatorService)))
+            {
+                host.Opened+=host_Opened;
+                host.Open();
+                Console.ReadLine();
+            }
+        }
+
+        private static void HostByCode()
+        {
+            using (ServiceHost host = new ServiceHost(typeof(CalculatorService)))
+            {
+                host.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "http://127.0.0.1:9999/CalculatorService");
+                if (host.Description.Behaviors.Find<ServiceMetadataBehavior>() == null)
+                {
+                    ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
+                    behavior.HttpGetEnabled = true;
+                    behavior.HttpGetUrl = new Uri("http://127.0.0.1:9999/CalculatorService/metadata");
+                    host.Description.Behaviors.Add(behavior);
+                }
+                host.Opened += host_Opened;
+                host.Open();
+                Console.ReadLine();
+
+            }
+        }
+
+        private static void host_Opened(object sender, EventArgs e)
+        {
+            Console.WriteLine("CalculatorService已经启动成功，按任意键终止服务");
+        }
+    }
+}
